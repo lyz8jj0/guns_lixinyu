44ef0e8bb376a2fed7d1976a3754e73949367a15
lyz8jj0 <844216765@qq.com>
2020-04-03 10:29:04 +0800

    guns_lixinyu项目搭建

---
+++ guns-rest/src/main/java/com/lixinyu/guns/rest/GunsRestApplication.java
@@ -0,0 +1,16 @@
+package com.lixinyu.guns.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author by 李新宇
+ * @date 2020-04-03 10:06
+ */
+@SpringBootApplication
+public class GunsRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GunsRestApplication.class, args);
+	}
+}
